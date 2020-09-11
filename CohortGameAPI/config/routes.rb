8899c90a4e7d6d215a42c08e7f8ffa819fa236cc
Nroulston <nicolas.roulston@gmail.com>
2020-09-11 12:38:21 -0700

    root route created

--- CohortGameAPI/config/routes.rb
+++ CohortGameAPI/config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  root 'game_rooms#index'
   resources :players
   resources :game_rooms
+
+  mount ActionCable.server, at: '/cable'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
