203296dd3d619d13e3fb4440c1e681e0995a0940
Nroulston <nicolas.roulston@gmail.com>
2020-09-12 11:06:00 -0700

    Action Cable Connection changed to  established on submmission of user name

--- CohortGameFrontEnd/src/index.js
+++ CohortGameFrontEnd/src/index.js
@@ -9,6 +9,18 @@ const HEADERS = {
 import ActionCable from 'actioncable'
 const cable = ActionCable.createConsumer(API_WS_ROOT)
 const body = () => document.querySelector("body")
+const inputForm = () => document.querySelector("#user_name")
+const submitName = () => { 
+ inputForm().addEventListener('keydown', function(e) {
+   if (e.keyCode == 13) {
+     e.preventDefault()
+     establishActionCableConnection()
+     console.log(e)
+   } 
+ })
+}
+
+
 document.addEventListener("DOMContentLoaded", function() {
   const div = document.createElement("div")
   const formDiv = document.createElement('div')
@@ -16,31 +28,35 @@ document.addEventListener("DOMContentLoaded", function() {
   const inputDiv = document.createElement("div")
   const inputField = document.createElement('input')
   const inputLabel = document.createElement('label')
-
-  inputField.placeholder = "Name"
-
+  inputField.placeholder = "Enter Name"
+  inputField.setAttribute("id", "user_name")
+  inputField.setAttribute("name", "user[name]")
+  inputLabel.setAttribute("for", "user_name")
+  inputLabel.innerText = "First Name"
   body().append(div)
   div.append(form)
   form.append(formDiv)
   formDiv.append(inputDiv)
-  inputDiv.append(inputField)
   inputDiv.append(inputLabel)
+  inputDiv.append(inputField)
+
+  submitName()
 
 })
 
 
+function establishActionCableConnection() {
+  cable.subscriptions.create('GameRoomChannel', {
+    connected() {
+      console.log("connected to the room")
+    },
 
-cable.subscriptions.create('GameRoomChannel', {
-  connected() {
-    console.log("connected to the room")
-  },
+    disconnected() {
+      // Called when the subscription has been terminated by the server
+    },
 
-  disconnected() {
-    // Called when the subscription has been terminated by the server
-  },
-
-  received(data) {
-    alert(data['message'])
-  },
-});
-
+    received(data) {
+      alert(data['message'])
+    },
+  });
+}
